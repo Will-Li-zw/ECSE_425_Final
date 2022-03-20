0cacb261deca54664a982236193841a172c4045f
Linwei Yuan <56415387+greyyuan211@users.noreply.github.com>
2022-03-20 17:32:40 -0400

    compile successfully

--- Pipelined_Processor/fetchState.vhd
+++ Pipelined_Processor/fetchState.vhd
@@ -1,3 +1,4 @@
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
@@ -18,6 +19,7 @@ entity fetchStage is
         m_addr: out integer range 0 to memory_size-1;
         m_read: out std_logic;
         m_readdata: in std_logic_vector (bit_width-1 downto 0);
+        m_waitrequest: in std_logic
         -- write is not needed in fetch stage
     );
 end fetchStage;
@@ -26,27 +28,32 @@ architecture arch of fetchStage is
     signal pc_register: integer range 0 to memory_size-1 := 0;
     signal pc_next_addr: integer range 0 to memory_size-1 := 0;
 begin
+
+
+-- trigger when sensitive list is triggered
+pc_process: process(clock,reset,pc_register,pc_next_addr,stall)
+begin
     pc <= pc_register;
+    -- updatee pc_next_addr
     if (reset='1') then
         pc_next_addr <= 0;
     elsif (pc_register + 4 >= memory_size-1) then --exceed a word
         pc_next_addr <= pc_register;
-    elsif (stall='1') else
+    elsif (stall='1') then
         pc_next_addr <= pc_register;
     else
         pc_next_addr <= pc_register + 4;
+    end if;
 
--- trigger when sensitive list is triggered
-pc_process: process(clock,reset,pc_next,stall)
-begin
+    -- update pc_register
     -- if want to reset
     if (reset='1') then
-        pc_register <=0
+        pc_register <=0;
     elsif (rising_edge(clock)) then
         if (stall = '1') then
             -- do nothing
         else
-            pc_register <= pc_next;
+            pc_register <= pc_next_addr;
         end if;
     end if;
 end process;
@@ -58,10 +65,10 @@ begin
     end if;
 end process;
 
-memory_process: process(clock, reset, pc_register,m_readdata,m_waitreqest)
+memory_process: process(clock, reset, pc_register,m_readdata,m_waitrequest)
 begin
     -- TODO: get instructions from memory
-    if reset = '1' then
+    -- if reset = '1' then
 
-
-end architecture
+end process;
+end architecture;
