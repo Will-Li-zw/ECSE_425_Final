781b03dd5da31e78d89fbe52796ce86ecd2f6f74
Jack Wei <59746876+jyiwei@users.noreply.github.com>
2022-03-26 18:36:39 -0400

    update write back stage formatting

--- Pipelined_Processor/Stages/writebackStage.vhd
+++ Pipelined_Processor/Stages/writebackStage.vhd
@@ -33,10 +33,10 @@ architecture Behavior of writebackStage is
 
     component TWOMUX is
         PORT(
-			sel : in std_logic;
-			input0 : in std_logic_vector (word_width-1 downto 0);
-			input1 : in std_logic_vector (word_width-1 downto 0);
-			output : out std_logic_vector (word_width-1 downto 0);
+		sel : in std_logic;
+		input0 : in std_logic_vector (word_width-1 downto 0);
+		input1 : in std_logic_vector (word_width-1 downto 0);
+		output : out std_logic_vector (word_width-1 downto 0);
        );
     end component;
 
