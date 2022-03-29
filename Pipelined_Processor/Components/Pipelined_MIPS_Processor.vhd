45450cda067f7b522780eaab06588e36adddb07e
Zichen <changzichen2000@gmail.com>
2022-03-29 15:52:52 -0400

    Pipelined_MIPS_processor pass compile

--- Pipelined_Processor/Components/Pipelined_MIPS_Processor.vhd
+++ Pipelined_Processor/Components/Pipelined_MIPS_Processor.vhd
@@ -8,18 +8,11 @@ library std;
 USE std.textio.all;						 -- package for io keywords: file_open()
 
 ENTITY Pipelined_MIPS_Processor IS
-	GENERIC(
-		inst_ram_size : INTEGER := 4096;			-- BYTE ADDRESSABILITY; at most 1024 instructions
+    generic(
+        inst_ram_size : INTEGER := 4096;			-- BYTE ADDRESSABILITY; at most 1024 instructions
 		data_ram_size : INTEGER := 32768;			-- BYTE ADDRESSABILITY; 8192 words data
 		word_size : INTEGER := 32;
-		byte_size : INTEGER := 8;
-		mem_delay : time := 1 ns;			-- to make life easier, mem_dealy is 0.1 CC
 		clock_period : time := 1 ns
-
-		-- path definition
-		dataoutput_filepath: string := "memory.txt";   -- only data memory output
-        registeroutput_filepath : string := "register_file.txt";
-		instruction_filepath : string := "program.txt"
 	);
 	PORT (
 		clock       : in std_logic;            -- top level clock
@@ -32,20 +25,37 @@ END Pipelined_MIPS_Processor;
 ARCHITECTURE behavior OF Pipelined_MIPS_Processor IS 
     -- components declaration
     component Processor is 
+    generic(
+        word_size : INTEGER := 32;
+        registeroutput_filepath : string := "register_file.txt"
+    );
     port(
         clock       : in std_logic;
-        instruction : in std_logic_vector(wordsize-1 downto 0);
-        read_data   : in std_logic_vector(wordsize-1 downto 0);
+        instruction : in std_logic_vector(word_size-1 downto 0);
+        read_data   : in std_logic_vector(word_size-1 downto 0);
         
         pc          : out integer;
         data_addr   : out integer;
         write_req   : out std_logic;
-        read_req    : out std_logic;
-        write_data  : out std_logic_vector(wordsize-1 downto 0)
+        instread_req : out std_logic;
+        dataread_req : out std_logic;
+        write_data  : out std_logic_vector(word_size-1 downto 0)
     );
     end component;
 
     component memory is 
+    generic(
+        inst_ram_size : INTEGER := 4096;			-- BYTE ADDRESSABILITY; at most 1024 instructions
+		data_ram_size : INTEGER := 32768;			-- BYTE ADDRESSABILITY; 8192 words data
+		word_size : INTEGER := 32;
+		byte_size : INTEGER := 8;
+		mem_delay : time := 1 ns;			-- to make life easier, mem_dealy is 0.1 CC
+		clock_period : time := 1 ns;
+
+		-- path definition
+		dataoutput_filepath: string := "memory.txt";   -- only data memory output
+		instruction_filepath : string := "program.txt"
+    );
     port(
         clock: in std_logic;
 		writedata: in std_logic_vector(word_size-1 DOWNTO 0);  -- pass in a WORD; However, only data memory can be written
@@ -67,14 +77,14 @@ ARCHITECTURE behavior OF Pipelined_MIPS_Processor IS
 
     -- local variables
     
-    signal instruction  : std_logic_vector (wordsize-1 downto 0);
-    signal read_data    : std_logic_vector (wordsize-1 downto 0);
+    signal instruction  : std_logic_vector (word_size-1 downto 0);
+    signal read_data    : std_logic_vector (word_size-1 downto 0);
     signal inst_addr    : integer range 0 to inst_ram_size-1;
     signal data_addr    : integer range 0 to data_ram_size-1;
     signal write_req    : std_logic;
-    signal instread_req : std_logic;        -- instruction read request
-    signal dataread_req : std_logic;        -- data read request
-    signal write_data   : std_logic_vector(wordsize-1 downto 0);
+    signal instread_req : std_logic := '0';        -- instruction read request
+    signal dataread_req : std_logic := '0';        -- data read request
+    signal write_data   : std_logic_vector(word_size-1 downto 0);
     signal wait_request : std_logic;
 
 BEGIN
@@ -95,16 +105,6 @@ BEGIN
     );
 
     mem : memory
-    generic map(
-        inst_ram_size   => inst_ram_size;			-- BYTE ADDRESSABILITY; at most 1024 instructions
-		data_ram_size   => data_ram_size;			-- BYTE ADDRESSABILITY; 8192 words data
-		word_size       => word_size;
-		byte_size       => byte_size;
-		mem_delay       => mem_delay;
-		clock_period    => clock_period;
-		dataoutput_filepath     => dataoutput_filepath
-		instruction_filepath    => instruction_filepath
-    );
     port map (
         clock       => clock,
         writedata   => write_data,
@@ -121,3 +121,5 @@ BEGIN
         memload     => memload,
         memoutput   => memoutput
     );
+
+END behavior;
--- Pipelined_Processor/Components/Processor.vhd
+++ Pipelined_Processor/Components/Processor.vhd
@@ -1,4 +1,5 @@
---Adapted from Example 12-15 of Quartus Design and Synthesis handbook
+--This processor entity will connect five stages together 
+--
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
@@ -7,9 +8,59 @@ USE ieee.std_logic_textio.all;			 -- package for io to write std_logic_vector
 library std;
 USE std.textio.all;						 -- package for io keywords: file_open()
 
-ENTITY Processor IS
+entity Processor is
 -- TODO: haven't finished
-	PORT (
-		clock       : in std_logic           -- top level clock
+	generic(
+		clock_period : time := 1 ns;
+		word_size : INTEGER := 32
 	);
-END Processor;
+	port (
+		clock       : in std_logic;
+        instruction : in std_logic_vector(wordsize-1 downto 0);
+        read_data   : in std_logic_vector(wordsize-1 downto 0);
+        
+        pc          : out integer;
+        data_addr   : out integer;
+        write_req   : out std_logic;
+        instread_req : out std_logic;
+        dataread_req : out std_logic;
+        write_data  : out std_logic_vector(wordsize-1 downto 0)
+	);
+end Processor;
+
+architecture behavior of Processor is 
+	-- components declaration
+	component fetch is 
+	port(
+		--
+	);
+	end component;
+
+	component decode is 
+	port(
+		--
+	);
+	end component;
+
+	component execute is 
+	port(
+		--
+	);
+	end component;
+
+	component memory is 
+	port(
+		--
+	);
+	end component;
+
+	component writeback is 
+	port(
+		--
+	);
+	end component;
+
+begin
+	-- 连连看:
+
+end behavior;
