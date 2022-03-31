0c106fb81e45902a786a138b53eb3779ee807e75
Linwei Yuan <56415387+greyyuan211@users.noreply.github.com>
2022-03-30 20:09:28 -0400

    update fetchStage

--- Pipelined_Processor/Stages/fetchStage.vhd
+++ Pipelined_Processor/Stages/fetchStage.vhd
@@ -1,96 +1,117 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-entity newFetchStage is
-    port (
-        clk: in std_logic;
-        reset: in std_logic;
-        stall: in std_logic:='0';
-        stall_number: in std_logic_vector(31 downto 0):=(others=>'0');
-        processor_enable: in std_logic:='0';
+entity fetchStage is
+    generic(
+        memory_size: integer := 8192; --data memory size is 8192 lines
+        bit_width: integer := 32
+    );
+
+    port(
+        clock: in std_logic; --required
+        reset: in std_logic; --required
+        stall: in std_logic; --required
 
         -- input from WB
-        if_jump_addr: in std_logic_vector (31 downto 0):=(others=>'0'); --TODO: from decode stage
-        if_branchAddr: in std_logic_vector (31 downto 0):=(others=>'0'); -- TODO: from memory step
-        if_ctrl_pcSrc: in std_logic := '0';
-        if_ctrl_jump: in std_logic := '0';
+        jump_addr: in std_logic_vector (31 downto 0):=(others=>'0'); --TODO: from decode stage
+        branch_addr: in std_logic_vector (31 downto 0):=(others=>'0'); -- TODO: from memory step
+        if_branch: in std_logic := '0'; -- if branch
+        if_jump: in std_logic := '0';
 
-        -- output
         pc: out STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
-        instruction_out: out STD_LOGIC_VECTOR (31 downto 0) := (others => '0')
+        pc_next: out STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
     );
-end newFetchStage;
+end fetchStage;
 
-architecture implementation of newFetchStage is
-    component TWOMUX is
-		Port (
-			sel : IN STD_LOGIC;
-			input0 : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-			input1 : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-			output : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
-		);
-	end component;
+architecture arch of fetchStage is
+    signal pc_register: STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal pc_next_register: STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    -- signal readbuffer: std_logic_vector (bit_width-1 downto 0);
 
-    component ADDER is
-        port (
-            X: in std_logic_vector(31 downto 0);
-            Y: in std_logic_vector(31 downto 0);
-            CIN: in std_logic;
-            COUT: out std_logic;
-            R: out std_logic_vector(31 downto 0)
-        );
-    end component;
+    -- component memory is
+    --     generic(
 
-    signal adder1_output: std_logic_vector(31 downto 0):=(others=>'0');
-    signal pc_register: std_logic_vector(31 downto 0):=(others=>'0');
-    signal mux_branch_output: std_logic_vector(31 downto 0):=(others=>'0');
-    signal temp_output: std_logic_vector(31 downto 0):=(others=>'0');
-    signal mux_jump_output: std_logic_vector(31 downto 0):=(others=>'0');
-	signal mux_stall_output: std_logic_vector(31 downto 0):=(others=>'0');
+    --         word_size : INTEGER := 32;
 
+    --     );
+    --     port(
+    --         clock: IN STD_LOGIC;
+    --         writedata: IN STD_LOGIC_VECTOR (word_size-1 DOWNTO 0);    -- WORD ADDRESSABILITY; However, only data memory can be written
+    --         memwrite: IN STD_LOGIC;							-- write reqeust for data
+            
+    --         inst_address: IN INTEGER RANGE 0 TO inst_ram_size-1;
+    --         data_address: IN INTEGER RANGE 0 TO data_ram_size-1;
+        
+    --         datamemread: IN STD_LOGIC;						-- read request for data
+    --         instmemread: IN STD_LOGIC;						-- read request for instruction
+        
+    --         readdata: OUT STD_LOGIC_VECTOR (word_size-1 DOWNTO 0);	-- WORD ADDRESSABILITY
+    --         waitrequest: OUT STD_LOGIC;
+        
+    --         memload: IN STD_LOGIC;			-- signal to load initial instructions from "program.txt"	
+    --         memoutput: IN STD_LOGIC			-- signal to write output file
+    --     );
+    --     end component;
 begin
 
-    -- TODO: MUX branch
-    mux_branch: TWOMUX port map(
-        sel => if_ctrl_pcSrc,
-        input0 => adder1_output,
-        input1 => if_branchAddr,
-        output => mux_branch_output
-    );
+-- dut: memory
+-- port map (
+--     clock=>clock,
+--     instmemread=>m_read,
+--     inst_address=>m_addr,
+--     readdata=>readbuffer
+--     -- waitrequest=>m_waitrequest,
+-- );
 
-    mux_jump : TWOMUX port map(
-	    sel => if_ctrl_jump,	
-        input0 => mux_branch_output,
-        input1 => if_jump_addr,
-        output => mux_jump_output
-	);
+-- change output
+pc <= pc_register;
+pc_next <= pc_next_register;
 
-    staller : TWOMUX port map(
-	    sel => stall,	
-        input0 => mux_jump_output,
-        input1 => stall_number,
-        output => mux_stall_output
-	);
+-- trigger when sensitive list is triggered
+pc_process: process(clock,reset,pc_register,pc_next_register,stall,if_branch,if_jump)
+begin
+    -- updatee pc_next_register
+    if (reset='1') then
+        pc_next_register := 0;
+    elsif (if_branch='1') then
+        pc_next_register := branch_addr;
+    elsif (if_jump='1') then
+        pc_next_register := jump_addr;
+    elsif (pc_register + 4 >= memory_size-1) then
+        pc_next_register := pc_register;
+    elsif (stall='1') then
+        pc_next_register := pc_register;
+    else
+        pc_next_register := pc_register + 4;
+    end if;
 
-    adder1: ADDER port map(
-        X=>pc_register,
-        Y=>"00000000000000000000000000000001",
-        CIN=>'0',
-        R=>adder1_output
-    );
-
-    fetch: process(clk,reset)
-    begin
-        if reset='1' or processor_enable='0' then
-            pc <= (others=>'0');
-            instruction_out <= (others=>'0');
-            pc_register<=(others=>'0');
-        elsif rising_edge(clk) then
-            pc_register <=mux_stall_output;
-            pc <= adder1_output;
-            instruction_out<=mux_stall_output;
+    -- update pc_register
+    -- if want to reset
+    if (reset='1') then
+        pc_register <=0;
+    elsif (rising_edge(clock)) then
+        if if_jump = '0' and if_branch = '0' then
+            pc_register <= pc_next_register;
+        else
+            
         end if;
-    end process;
-end implementation; 
+    end if;
 
+end process;
+
+-- memory_process: process(clock, reset, pc_register,m_readdata,stall)
+-- begin
+--     -- TODO: get instructions from memory
+--     if reset = '1' or stall = '1' then
+--         -- do nothing
+--         instruction <= x"ffffffff"; -- garbage output for stall to notify decode and execute stage
+--     elsif rising_edge(clock) then
+--         m_read <= '1';
+--         m_addr <= pc_register;
+--         instruction <= readbuffer;
+
+--     end if;
+
+-- end process;
+end architecture;
